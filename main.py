05b0396a85795f85ef6ac734e3ac66c9096aad9e
HPOA909 <main.hpoa.discord@gmail.com>
2024-05-21 18:52:03 -0700

    Project began w/ progress

---
+++ main.py
@@ -0,0 +1,15 @@
+# Import the required libraries
+import sqlite3 as sql
+from tkinter import *
+from PIL import ImageTk, Image
+
+# Start the program
+# Define functions here
+
+def addRecord
+
+
+# Get the properties
+main = Tk()
+main.title("Tkinter CRUD Project")
+main.geometry("800x800")
