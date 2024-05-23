328c7b4d263a205b10b7b7f07778a2a74703911c
HPOA909 <main.hpoa.discord@gmail.com>
2024-05-23 12:03:58 -0700

    May continue to develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import the required libraries
-import psycopg as psql
+import psycopg2 as psql
+import psycopg2.sql as sql
 from tkinter import *
 from PIL import ImageTk, Image
 
@@ -11,15 +12,13 @@ path = "/home/howard/Documents/Projects/CISP71_MTSAC_SP2024-Tkinter_CRUD/data/"
 # Add the addRecord function
 def addRecord():
     # connect to the DB via Postgres SQL
-    conn = psql.connect(database = "Quizzing", 
+    conn = psql.connect(dbname = "Quizzing", 
                         user = "postgres", 
-                        host= 'localhost',
-                        password = "postgres",
-                        port = 5432)
+                        host= "localhost",
+                        password = "postgres")
     try:
         c=conn.cursor()
-        c.execute("insert the user's contacts values (?,?,?)", 
-                  (fNameEN.get(), lNameEN.get(), uNameEN.get()))
+        c.execute()
         conn.commit()
         print("Your record has added successfully")
     except:
@@ -31,8 +30,11 @@ def addRecord():
 # Get the properties
 main = Tk()
 main.title("Tkinter CRUD Project")
-main.geometry("800x800")
+main.geometry("400x400")
 main.iconbitmap()
+
+# Create the button(s)
+addBT=Button(main, text="Add Record", command=addRecord)
 
 # Create labels
 fNameLB=Label(main,text="First Name")
@@ -43,3 +45,19 @@ uNameLB=Label(main,text="User Name")
 fNameEN=Entry(main)
 lNameEN=Entry(main)
 uNameEN=Entry(main)
+
+# Specify the label grids
+fNameLB.grid(row=0, column=0)
+lNameLB.grid(row=1,column=0)
+uNameLB.grid(row=2, column=0)
+
+#Specify the entry grids
+fNameEN.grid(row=0, column=1)
+lNameEN.grid(row=1, column=1)
+uNameEN.grid(row=2, column=1)
+
+# Specify the button grids
+addBT.grid(row=4, column=0)
+
+# Call the mainloop
+main.mainloop()
